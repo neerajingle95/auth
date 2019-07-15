06c551230e39ed1d83a4f0783b42d4e304aab13b
Neeraj Ingle <neerajingle95@gmail.com>
2019-07-15 17:21:36 +0530

    Add files via upload

---
+++ server-backup/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// DB Setup
+mongoose.connect("mongodb://localhost:auth/auth", { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.on("connected", function() {
+  console.log("connected to db");
+});
+
+// App Setup
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
